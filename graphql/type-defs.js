831180018e3099b7d505cfba43fb261eba813f0a
rcloutieruf <ryancloutier@ufl.edu>
2024-04-22 01:11:21 -0400

    Club implementation based on user implementation

--- graphql/type-defs.js
+++ graphql/type-defs.js
@@ -19,6 +19,22 @@ const typeDefs = gql`
         category: String!
     }
 
+    type Event {
+        id: ID!
+        title: String!
+        description: String!
+        date: String!
+    }
+
+        # (1) make the Club a separate entity or (2) make it a user with extra privileges?
+        type Club{
+            id: ID!
+            username: String!
+            email: String!
+            password: String!
+            token: String!
+            events: [Event]
+        }
 
     input RegisterInput {
         username: String!
@@ -30,12 +46,17 @@ const typeDefs = gql`
     type Query{
         getUsers: [User!]!
         getAssignmentsByDue(target_username: String!, target_dueDate: String!): [Assignment]!
+        getClubs: [Club!]!
+        getEventsByDue(target_username: String!, target_dueDate: String!): [Event]!
     }
 
     type Mutation{
         registerUser(registerInput: RegisterInput): User!
         loginUser(username: String!, password: String!): User!
-        addAssignment(title: String!, description: String! dueDate: String!, category: String!): User!   #dueDate formatted as xx/xx/xxxx
+        addAssignment(title: String!, description: String!, dueDate: String!, category: String!): User!   #dueDate formatted as xx/xx/xxxx
+        registerClub(registerInput: RegisterInput): Club!
+        loginClub(username: String!, password: String!): Club!
+        addEvent(title: String!, description: String!, dueDate: String!, category: String!): Club!
     }
 `;
 
