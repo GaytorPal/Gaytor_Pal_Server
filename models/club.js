0c48ebd173dc6fd7276444b346712e669d0d7c8d
Leonardo D. Cobaleda <116767849+leonardocoba@users.noreply.github.com>
2024-04-24 01:50:41 -0400

    added manual club add and retrieve

--- models/club.js
+++ models/club.js
@@ -1,21 +1,22 @@
-const { model, Schema } = require('mongoose');
+const { model, Schema } = require("mongoose");
 
 clubSchema = new Schema({
-    username: String,
-    password: String,
-    email: String,
-    createdAt: String,
-    club_name: String,
-    category: String,
-    events: [
-        {
-            title: String,
-            description: String,
-            dueDate: String,
-            dateReduced: String
-        }
-    ],
-    followers: [{ type: Schema.Types.ObjectId, ref: 'User' }]
-  });
+  username: String,
+  password: String,
+  email: String,
+  createdAt: String,
+  club_name: String,
+  category: [String],
+  description: String,
+  events: [
+    {
+      title: String,
+      description: String,
+      dueDate: String,
+      dateReduced: String,
+    },
+  ],
+  followers: [{ type: Schema.Types.ObjectId, ref: "User" }],
+});
 
-module.exports = model('Club', clubSchema);
+module.exports = model("Club", clubSchema);
